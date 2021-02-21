641bf4bdda986276b945ac3aef2cd0e7fb3547ad
Luciano Silva Nascimento <luciano.silva@engie.com>
2021-02-20 23:14:51 -0300

    add gRPC server and client

---
+++ cmd/client/client.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/luciano-nascimento/grpc-go-lab/pb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	connection, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Could not connect to gRPC Server: %v", err)
+	}
+	//close connection when not used anymore
+	defer connection.Close()
+
+	client := pb.NewUserServiceClient(connection)
+	AddUser(client)
+}
+
+func AddUser(client pb.UserServiceClient) {
+	req := &pb.User{
+		Id:    "0",
+		Name:  "Luciano",
+		Email: "l@l.com",
+	}
+
+	res, err := client.AddUser(context.Background(), req)
+	if err != nil {
+		log.Fatalf("Could not make gRPC request: %v", err)
+	}
+
+	fmt.Println(res)
+}
--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/luciano-nascimento/grpc-go-lab
 go 1.14
 
 require (
-	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/golang/protobuf v1.4.3
+	google.golang.org/grpc v1.34.0
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.1.0 // indirect
-	google.golang.org/protobuf v1.25.0 // indirect
+	google.golang.org/protobuf v1.25.0
 )
